e41299ac93175f1772c235def295ffbfe0e858a8
tituscarl <titusranario@gmail.com>
2025-11-09 18:19:49 +0800

    ci skip: use args on purge

--- subcmds/topic/purge.go
+++ subcmds/topic/purge.go
@@ -15,26 +15,29 @@ var topic string
 
 func PurgeCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "purge",
+		Use:   "purge <topic>",
 		Short: "Purge all messages from a pubsub topic",
 		Long:  `Purge all messages from a pubsub topic.`,
 		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) == 0 || args[0] == "" {
+				logger.Fail("Topic is required")
+				return
+			}
 			client, err := pubsub.New(pubsub.WithLogger(log.New(io.Discard, "", 0)))
 			if err != nil {
-				cmd.Println("Error creating pubsub client:", err)
+				logger.Fail("Failed to create pubsub client:", err)
 				return
 			}
 			defer client.Close()
-			c, err := client.PurgeTopic(context.Background(), topic)
+			c, err := client.PurgeTopic(context.Background(), args[0])
 			if err != nil {
 				logger.Fail("Failed to purge topic:", err)
 				return
 			}
-			logger.Info(fmt.Sprintf("Successfully purged %d messages from topic: %s", c, topic))
+
+			logger.Info(fmt.Sprintf("Successfully purged %d messages from topic: %s", c, args[0]))
 		},
 	}
 
-	cmd.Flags().StringVar(&topic, "topic", "", "The topic to purge messages from (required)")
-	cmd.MarkFlagRequired("topic")
 	return cmd
 }
